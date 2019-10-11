c1edab491801ca39068dbb30be242350cdb717e9
Cerdic <cedric@yterium.com>
2019-10-11 13:02:18 +0200

    up plugin factures

--- plugins/deploy.sh
+++ plugins/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-checkout.php git -r2bf4c8c3  https://github.com/nursit/factures.git externals/factures
+checkout.php git -re22a7bf -bmaster  https://github.com/nursit/factures.git externals/factures
